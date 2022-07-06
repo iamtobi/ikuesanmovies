72a2ddd6615a8d2368954017e0fa63850b67fb48
Oluwatobi Ikuesan <88734402+iamtobi@users.noreply.github.com>
2022-07-06 14:03:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const hdbtn = document.querySelector('#hdbtn');
 const ntbtn = document.querySelector('#ntbtn');
 var home = document.querySelector('#homebtn');
 const links = ["https://c.mandela.h.sabishare.com/dl/rFJsJcwZd03/41f30f066b5985d6cf73caea81eaa5374cc65c16b30187d98f3b97364d9ec09d/Encanto_(2021)_(NetNaija.com).mp4"];
-alert(typeof(links));
 const optionbtn = document.querySelector('.Optionbtn');
 function showContentYt(){
     var ytcnt = document.querySelector('#ytcnt');
